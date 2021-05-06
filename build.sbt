4bde36077a4d4cfde174b13cff9702570f09122e
NDYALIVANAZ <ndyalivanaz@cput.ac.za>
2021-05-07 01:22:37 +0200

    a second exercise

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "ExcerciseTwo"
+
+version := "0.1"
+
+scalaVersion := "2.13.5"
---
+++ src/main/scala/SecondExcercise.scala
@@ -0,0 +1,12 @@
+
+
+def main(args: Array[String]) {
+  val sum = (a: Int, b: Int) => a + b
+  val subtract = (a: Double, b: Double) => a / b
+
+  print(" The sum of two numbers : " + sum(2, 3))
+  print(" The difference of two numbers is : " + sum(2, 3))
+  print(" The difference of two numbers is : " + subtract(2.5, 3.6))
+
+
+}
